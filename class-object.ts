7caafbe9c7eb97113dd392c336b1fe4d53400afa
hrhabib07 <mdhabibur.hr7@gmail.com>
2026-01-23 16:15:29 +0600

    comment added with help of AI for better understanding in the future

--- class-object.ts
+++ class-object.ts
@@ -1,15 +1,24 @@
 {
+  // Class = blueprint for creating objects
   class Animal {
+    // Constructor runs automatically when a new object is created
+    // 'public' here automatically creates and assigns properties
     constructor(
-      public name: string,
-      public species: string,
-      public sound: string,
+      public name: string, // name of the animal
+      public species: string, // species type
+      public sound: string, // sound the animal makes
     ) {}
+
+    // Method (function inside a class)
+    // This defines behavior of the Animal object
     makeSound() {
       console.log(`Our ${this.name} bhai says ${this.sound}`);
     }
   }
 
-  const person1 = new Animal("Hifjur", "MUian", "Ha ha ");
+  // Creating an object (instance) from the Animal class
+  const person1 = new Animal("Hifjur", "Mammal", "Ha ha");
+
+  // Calling a method on the object
   person1.makeSound();
 }
